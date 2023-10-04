bdce2fc3f6e550dda464d19eb9950296aef0fde7
Mahadev Sunil <mahadevsunil0@gmail.com>
2023-10-04 20:16:20 +0530

    About page is modified

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function About() {
   return (
+    <div>
     <div style={{color:"white",textAlign:'center'}}><h1>About</h1></div>
+    <h4 style={{textAlign:'center', justifyItems:'center', marginTop:'20%', color:'white'}} >Created by <span style={{fontWeight:'bold'}}> Mahadev Sunil</span></h4>
+    </div>
   )
 }
 
